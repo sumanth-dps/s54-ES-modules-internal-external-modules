dbfed50775c02d93d42417a3b3c7e02ba6a35a7c
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2024-09-01 19:21:12 +0530

    Add files via upload

---
+++ s54-es modules internal and external modules/kalki.js
@@ -0,0 +1,13 @@
+let movieName = "Kalki 2898 AD";
+let movieDirector = "Nag Ashwin";
+let movieCast = [
+  "Prabhas",
+  "Deepika Padukone",
+  "Kamal Hassan",
+  "Amitabh Bachan",
+];
+let aboutKalki = () => {
+  console.log("Kalki 2898 AD movie collected more than 1000crs");
+};
+let movieBudget = 6000000000;
+module.exports = { movieName, movieDirector, movieCast, aboutKalki };
---
+++ s54-es modules internal and external modules/server.js
@@ -0,0 +1,11 @@
+let KalkiMovieObj = require("./kalki");
+console.log(KalkiMovieObj.movieName);
+console.log(KalkiMovieObj.movieDirector);
+console.log(KalkiMovieObj.movieCast);
+KalkiMovieObj.aboutKalki();
+
+// let { movieName, movieDirector, movieCast, aboutKalki } = require("./kalki");
+// console.log(movieName);
+// console.log(movieDirector);
+// console.log(movieCast);
+// aboutKalki();
